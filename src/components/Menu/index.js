7c300dee6a3c708e147b4edcf478254ffe9d4b21
Guilherme Andreoti <guilherme.andreoti@outlook.com>
2020-08-05 18:19:52 -0300

    Added a navigator

--- src/components/Menu/index.js
+++ src/components/Menu/index.js
@@ -1,11 +1,17 @@
 import React from 'react'; 
-
+import Logo from '../../assets/img/logo.png';
+import './Menu.css';
 
 function Menu(){
     return(
-        <header>
-            TechFlix!
-        </header>
+        <nav className="Menu">
+            <a href="/home">
+                <img className='Logo' src={Logo} alt = 'Logo'></img>
+            </a>
+            <a className="ButtonLink" href="/">
+                Novo Video
+            </a>
+        </nav>
     );
 }
 
