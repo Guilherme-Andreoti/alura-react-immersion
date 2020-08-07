81e82a3b7489f6ecd313de4fcb0ac3d655158b63
Guilherme Andreoti <guilherme.andreoti@outlook.com>
2020-08-07 13:21:04 -0300

    template updated

--- src/components/Template/index.js
+++ src/components/Template/index.js
@@ -2,11 +2,11 @@ import React from 'react';
 import Menu from '../Menu';
 import Footer from '../Footer';
 
-function PageTemplate(){
+function PageTemplate({children}){
     return(
         <div>
             <Menu/>
-                
+                {children}
             <Footer/>
         </div>
 
