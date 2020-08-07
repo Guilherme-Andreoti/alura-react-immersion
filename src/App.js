36d60ff01bc2772f96bba200febf63ce272f1e3c
Guilherme Andreoti <guilherme.andreoti@outlook.com>
2020-08-07 12:14:28 -0300

    Updated the mainscreen

--- src/App.js
+++ src/App.js
@@ -1,11 +1,44 @@
 import React from 'react';
 import './App.css';
 import Menu from './components/Menu'
+import BannerMain from './components/BannerMain'
+import Carousel from './components/Carousel'
+import Footer from './components/Footer'
+import sampleData from './assets/data/dados_iniciais.json' 
+
 
 
 function App() {
   return (
+    <div>
     <Menu/>
+
+      <BannerMain
+        videoTitle={sampleData.categorias[0].videos[0].titulo}
+        url={sampleData.categorias[0].videos[0].url}
+        videoDescription={"Teste"}
+      />
+
+      <Carousel 
+        ignoreFirstVideo
+        category={sampleData.categorias[0]}
+      />
+
+      <Carousel 
+        ignoreFirstVideo
+        category={sampleData.categorias[1]}
+      />
+
+      <Carousel 
+        ignoreFirstVideo
+        category={sampleData.categorias[2]}
+      />
+
+     <Footer/>
+    </div>
+
+   
+
   );
 }
 
