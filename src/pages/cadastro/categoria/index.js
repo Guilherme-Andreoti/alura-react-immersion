3ee1a0b72ca409433682935d7c2f898ffc77ac9a
Guilherme Andreoti <guilherme.andreoti@outlook.com>
2020-08-07 20:27:51 -0300

    Made a form

--- src/pages/cadastro/categoria/index.js
+++ src/pages/cadastro/categoria/index.js
@@ -1,12 +1,54 @@
-import React from 'react';
+import React, {useState} from 'react';
 import Template from '../../../components/Template';
 import { Link } from 'react-router-dom';
 
 function CadastroCategoria(){
+    //Extrai o conteudo do useState
+    const [categorias,SetCategoias] = useState(['Teste']);
+    let [nomeCategoria,setNomeCategoria] = useState("Filmes")
     return (
         <>
           <Template>
-            <h1>Cadastro de Categoria</h1>
+            <h1>Cadastro de Categoria: {nomeCategoria}</h1>
+              <form onSubmit={
+                function  handleSubmit(eventInfo){
+                  eventInfo.preventDefault();
+                  SetCategoias([
+                    ...categorias,
+                    nomeCategoria
+                  ])
+                }
+              }>
+                <label>
+                  Nome da Categoria:
+                  
+                  <input 
+                    type="text" 
+                    value={nomeCategoria} 
+                    onChange={(info)=>{setNomeCategoria(info.target.value)}} 
+                  />
+                </label>
+                  <button>
+                    Cadastrar
+                  </button>
+              </form>
+                <ul>
+                  {
+                    categorias.map((categoria)=>{
+                      return(
+                        <li key={categoria}>
+                          {categoria}
+                        </li>
+                      )
+                    }
+                    )
+                  }
+                </ul>
+
+
+              
+
+
           <Link to="/">
               Ir para Home
           </Link>
