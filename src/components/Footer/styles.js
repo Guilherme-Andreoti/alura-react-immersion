7e99fac714835c2c3929c842d24a43a73b889efe
Guilherme Andreoti <guilherme.andreoti@outlook.com>
2020-08-07 13:39:55 -0300

    Now the footer is fixed

--- src/components/Footer/styles.js
+++ src/components/Footer/styles.js
@@ -7,9 +7,11 @@ export const FooterBase = styled.footer`
   padding-left: 16px;
   padding-right: 16px;
   padding-top: 32px;
-  padding-bottom: 32px;
   color: var(--white);
   text-align: center;
+  width: 100%;
+  bottom: 0;
+  position: fixed;
   @media (max-width: 800px) {
     margin-bottom: 50px;
   }
