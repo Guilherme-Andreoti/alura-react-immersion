2a468b0f8fdd4aeed847f9b704d9bcf0bf6f3289
Guilherme Andreoti <guilherme.andreoti@outlook.com>
2020-08-07 20:27:33 -0300

    Foote Update

--- src/components/Footer/index.js
+++ src/components/Footer/index.js
@@ -4,15 +4,9 @@ import { FooterBase } from './styles';
 function Footer() {
   return (
     <FooterBase>
-      <a href="https://www.alura.com.br/">
-        <img src="https://www.alura.com.br/assets/img/alura-logo-white.1570550707.svg" alt="Logo TechFlix" />
-      </a>
       <p>
-        Orgulhosamente criado durante a
-        {' '}
-        <a href="https://www.alura.com.br/">
-          Imersão React da Alura
-        </a>
+      Desenvolvido por 
+        <a href="https://br.linkedin.com/in/guilherme-andreoti-546761178"> Guilherme Andreoti</a>
       </p>
     </FooterBase>
   );
