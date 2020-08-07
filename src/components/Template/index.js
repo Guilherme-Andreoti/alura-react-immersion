2e6bd856258381b3552d42fc18c45c218ff4d3b1
Guilherme Andreoti <guilherme.andreoti@outlook.com>
2020-08-07 13:10:03 -0300

    Added a page template

---
+++ src/components/Template/index.js
@@ -0,0 +1,16 @@
+import React from 'react'; 
+import Menu from '../Menu';
+import Footer from '../Footer';
+
+function PageTemplate(){
+    return(
+        <div>
+            <Menu/>
+                
+            <Footer/>
+        </div>
+
+    )
+}
+
+export default PageTemplate;
